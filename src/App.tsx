a3a590621434909d47c7cb829d70b14a47d03a38
Anna Llorens <anna.llorens@symphony.com>
2022-05-23 19:29:34 +0200

    Added sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import './App.css';
+import './App.scss'; 
 import { Link, Route, Routes } from 'react-router-dom';
 import VirtualList from './components/VirtualList';
+import StateHooks from './components/StateHooks';
 
 const App = () => 
 <div className="App">
 
 <Routes>
   <Route path="/" element={<Home />} />
-  <Route path="about" element={<About />} />
   <Route path="virtual-list" element={<VirtualList />} />
+  <Route path="react-state-hooks" element={<StateHooks />} />
+  <Route path="about" element={<About />} />
 </Routes>
 </div>
 
@@ -21,6 +22,7 @@ const Home = (props: any) =>
 <nav className='app-header'>
   <Link to="/">Home</Link>
   <Link to="/virtual-list">Virtualized List</Link>
+  <Link to="/react-state-hooks">React state Hooks</Link>
   <Link to="/about">About</Link>
 </nav>
 </>
