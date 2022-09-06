0c13cae2aa48909aac061063617f1497243e1ece
Anna Llorens <anna.llorens@symphony.com>
2022-09-06 16:44:55 +0200

    Reorder components

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,11 @@ const App = () =>
 </div>
 
 const Home = (props: any) => 
-<>
-<main>
-<h1 className='mt-1 app-title'>Mi app!</h1>
-</main>
 <nav className='app-header'>
   <Link to="/">Home</Link>
   <Link to="/virtual-list">Virtualized List</Link>
   <Link to="/react-state-hooks">React state Hooks</Link>
   <Link to="/about">About</Link>
 </nav>
-</>
 
 export default App;
--- src/pages/Hooks/HooksPage.tsx
+++ src/pages/Hooks/HooksPage.tsx
@@ -1,41 +1,11 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
-type MyButtonProps = {
-  children: string;
-  onClick: () => void;
-};
-
-const MyButton = ({ children, onClick }: MyButtonProps): JSX.Element => (
-  <button onClick={onClick}>{children}</button>
-);
+import Counter from "./Counter";
 
 export default function HooksPage() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState('');
-  const [surName, setSurName] = useState('');
-  
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  });
-  return (
-    <div className="hooks-page">
-      <div className="app-header">
-        <h2>React Hooks</h2>
-      </div>
-
-      <div>
-        <h3>1. UseState</h3>
-        <h4>Counter example</h4>
-        <div>
-          <button className="mr-8" onClick={() => setCount(count - 1)}>-</button>
-          <button onClick={() => setCount(0)}>{count}</button>
-          <button className="ml-8" onClick={() => setCount(count + 1)}>+</button>
-        </div>
-        <h3 className="mt-42">2. UseEffect</h3>
-       
-      </div>
-    </div>
+  return (<>
+    <h2>React Hooks</h2>
+    <h3 className="mt-42">1. UseState</h3>
+    <Counter />
+    <h3 className="mt-42">2. UseEffect</h3>
+  </>
   );
 }
